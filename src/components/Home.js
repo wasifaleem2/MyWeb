25cb19e6ff5671d25615f06d0227244dedc9fad3
Wasif Aleem <wasifaleem2@gmail.com>
2022-10-10 15:37:49 +0100

    ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
+import { mobileStepperClasses } from "@mui/material";
 
 export default function Home() {
   return (
     <div className="home">
       <div className="home-content">
-        <h1>Wasif Aleem</h1>
+        <h1>WASIF ALEEM</h1>
         {/* <Link to="/about">Explore</Link> */}
       </div>
     </div>
